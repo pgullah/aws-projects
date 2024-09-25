689aa034b3b8626e4c7a53fabd61dc9c955ea56b
pgullah <pradeep.gulla@hotmail.com>
2024-09-25 15:07:19 +0100

    Fixed ec2 automation

--- emp-directory-app/.includes/ec2.sh
+++ emp-directory-app/.includes/ec2.sh
@@ -7,7 +7,9 @@ function _get_ec2_instance_by_name_state() {
 }
 
 function _get_ec2_instance_state() {
-    echo $(aws ec2 describe-instance-status --instance-id $1 --query "InstanceStatuses[*].InstanceState.Name")
+    # this doesn't return status when the instance stopped or terminated so utilizing ec2 describe-instances instead
+    # echo $(aws ec2 describe-instance-status --instance-id $1 --query "InstanceStatuses[*].InstanceState.Name")
+    aws ec2 describe-instances --instance-ids $1 --query 'Reservations[*].Instances[*].State.Name'
 }
 
 # IMAGE_ID, INSTANCE_TYPE, SECURITY_GROUP, SUBNET1_CIDR, INSTANCE_COUNT, INSTANCE_NAME, user_data
@@ -45,17 +47,17 @@ function _wait_for_ec2_instance_state() {
     instance_id=$1
     target_state=$2
     # defaults to 1 sec delay
-    sleep_delay=${3:-1}
+    sleep_delay=${3:-5}
     # [ -z $instance_id ] && echo 'Please specify instanceId'; return 1;
     # [ -z $target_state ] && echo 'Please specify the target state'; return 1;
 
-    current_state=$(_get_ec2_instance_state $instance_id)
-    echo ">>>>>>>>>> current_state: $current_state and target state: ${target_state}"
-    # while [ "${current_state}" != "${target_state}" ];
-    # do
-    #     echo "waiting for instance:$instance_id to be ${target_state}"
-    #     sleep ${sleep_delay}
-    #     current_state=$(_get_ec2_instance_state $instance_id)
-    #     echo ">>>>>>>>>> current_state: $current_state and target state: ${target_state}"
-    # done
+    # current_state=$(_get_ec2_instance_state $instance_id)
+    # echo ">>>>>>>>>> current_state: $current_state and target state: ${target_state}"
+    while [ "${current_state,,}" != "${target_state,,}" ];
+    do
+        echo "waiting for instance:$instance_id state to be ${target_state}"
+        sleep ${sleep_delay}
+        current_state=$(_get_ec2_instance_state $instance_id)
+        # echo ">>>>>>>>>> current_state: $current_state and target state: ${target_state}"
+    done
 }
